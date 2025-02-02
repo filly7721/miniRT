33e5f76947e201a2ab74636bc94cd3ce12d0d964
filly7721 <hmooodshalaab@gmail.com>
2025-02-02 09:05:35 +0400

    rewrote ambient to have color component

--- shapes_color.c
+++ shapes_color.c
@@ -26,14 +26,13 @@ double	get_brightness(t_minirt *minirt, t_tuple hit_point, t_tuple normal)
 	intersects = intersect(&ray, minirt->env->shapes);
 	if (intersects && closest_hit(intersects)->t
 		< mag_tuple(sub_tuples(hit_point, ray.origin)))
-		return (free_intersections(intersects), minirt->env->ambient.intensity);
+		return (free_intersections(intersects), 0);
 	free_intersections(intersects);
 	brightness = dot_tuple(mul_tuple(ray.direction, -1), normal);
 	if (brightness < 0)
 		brightness = 0;
 	return (brightness * minirt->env->light.bright
-		* (1.0 - minirt->env->ambient.intensity)
-		+ minirt->env->ambient.intensity);
+		* (1.0 - minirt->env->ambient.intensity));
 }
 
 t_tuple	get_sphere_color(t_minirt *minirt, t_ray *ray, t_intersection *inter, \
@@ -42,12 +41,21 @@ t_tuple	get_sphere_color(t_minirt *minirt, t_ray *ray, t_intersection *inter, \
 	t_tuple	albedo;
 	t_tuple	hp;
 	t_tuple	normal;
+	t_tuple	ambient;
 
 	albedo = set_tuple(sp->r / 255.0, sp->g / 255.0, sp->b / 255.0, 0);
+	ambient = set_tuple(minirt->env->ambient.r / 255.0,
+			minirt->env->ambient.g / 255.0,
+			minirt->env->ambient.b / 255.0, 0);
+	ambient = mul_tuple(ambient, minirt->env->ambient.intensity);
+	ambient.x *= albedo.x;
+	ambient.y *= albedo.y;
+	ambient.z *= albedo.z;
 	hp = add_tuples(mul_tuple(ray->direction, inter->t), ray->origin);
 	normal = normalize_tuple(sub_tuples(hp, set_point(sp->x, sp->y, sp->z)));
 	hp = add_tuples(hp, mul_tuple(ray->direction, -EPSILON));
-	return (mul_tuple(albedo, get_brightness(minirt, hp, normal)));
+	return (add_tuples(
+			mul_tuple(albedo, get_brightness(minirt, hp, normal)), ambient));
 }
 
 t_tuple	get_cylinder_color(t_minirt *minirt, t_ray *ray, t_intersection *inter, \
@@ -57,16 +65,25 @@ t_tuple	get_cylinder_color(t_minirt *minirt, t_ray *ray, t_intersection *inter, 
 	t_tuple	hp;
 	t_tuple	normal;
 	t_ray	newray;
+	t_tuple	ambient;
 
+	albedo = set_tuple(cy->r / 255.0, cy->g / 255.0, cy->b / 255.0, 0);
+	ambient = set_tuple(minirt->env->ambient.r / 255.0,
+			minirt->env->ambient.g / 255.0,
+			minirt->env->ambient.b / 255.0, 0);
+	ambient = mul_tuple(ambient, minirt->env->ambient.intensity);
+	ambient.x *= albedo.x;
+	ambient.y *= albedo.y;
+	ambient.z *= albedo.z;
 	newray.direction = mult_mat_tuple(&ray->direction, &cy->transform);
 	newray.origin = mult_mat_tuple(&ray->origin, &cy->transform);
-	albedo = set_tuple(cy->r / 255.0, cy->g / 255.0, cy->b / 255.0, 0);
 	hp = add_tuples(mul_tuple(newray.direction, inter->t), newray.origin);
 	normal = normalize_tuple(set_vector(hp.x, 0, hp.z));
 	normal = mult_mat_tuple(&normal, &cy->tp_transform);
 	hp = add_tuples(mul_tuple(ray->direction, inter->t), ray->origin);
 	hp = add_tuples(hp, mul_tuple(ray->direction, -EPSILON));
-	return (mul_tuple(albedo, get_brightness(minirt, hp, normal)));
+	return (add_tuples(
+			mul_tuple(albedo, get_brightness(minirt, hp, normal)), ambient));
 }
 
 t_tuple	get_plane_color(t_minirt *minirt, t_ray *ray, t_intersection *inter, \
@@ -75,12 +92,21 @@ t_tuple	get_plane_color(t_minirt *minirt, t_ray *ray, t_intersection *inter, \
 	t_tuple	albedo;
 	t_tuple	hp;
 	t_tuple	normal;
+	t_tuple	ambient;
 
 	albedo = set_tuple(pl->r / 255.0, pl->g / 255.0, pl->b / 255.0, 0);
+	ambient = set_tuple(minirt->env->ambient.r / 255.0,
+			minirt->env->ambient.g / 255.0,
+			minirt->env->ambient.b / 255.0, 0);
+	ambient = mul_tuple(ambient, minirt->env->ambient.intensity);
+	ambient.x *= albedo.x;
+	ambient.y *= albedo.y;
+	ambient.z *= albedo.z;
 	normal = normalize_tuple(set_vector(pl->norm_x, pl->norm_y, pl->norm_z));
 	hp = add_tuples(mul_tuple(ray->direction, inter->t), ray->origin);
 	hp = add_tuples(hp, mul_tuple(ray->direction, -EPSILON));
-	return (mul_tuple(albedo, get_brightness(minirt, hp, normal)));
+	return (add_tuples(
+			mul_tuple(albedo, get_brightness(minirt, hp, normal)), ambient));
 }
 
 t_tuple	get_color(t_minirt *minirt, t_ray *ray, t_intersection *intersection)
